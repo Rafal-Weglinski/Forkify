b80cd4597526397a9ce5ed8e2b232590292320f7
Rafal-Weglinski <52180356+Rafal-Weglinski@users.noreply.github.com>
2021-07-13 01:48:56 +0200

    Production build

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '0720417c-b352-4937-a396-50d7f18bcc70';
+export const KEY = 'e8420bed-9967-4331-bb8b-438a1108014a';
 export const MODAL_CLOSE_SEC = 2.5;
